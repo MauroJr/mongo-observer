96d3f99b5ce3b57d7eccfac06eb3dc05d8d8d24c
Mauro Junior <mauropereirajr@gmail.com>
2016-03-10 15:26:41 -0300

    removed index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-    create: require('./lib/mongo-observer').create,
-    getMongoConnection: require('./lib/get-db-connection'),
-    ObjectID: require('mongodb').ObjectID
-};
