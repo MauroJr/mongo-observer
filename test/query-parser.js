45c2eb5bb06e26c659d5dbc4ecdca76a949a76de
Mauro Junior <mauropereirajr@gmail.com>
2016-03-15 13:08:52 -0300

    upadted tests

--- test/query-parser.js
+++ test/query-parser.js
@@ -1,6 +1,25 @@
-const debug = require('debug')('test:query-parser'),
+'use strict';
+
+const chai = require('chai'),
+      expect = chai.expect,
       QueryParser = require('../lib/query-parser');
 
-const query = QueryParser({name: 'john doe'});
-
-console.log(query.match({name: 'john de'}));
+describe('Query Parser', function () {
+    
+    describe('simple query without operators', function () {
+        let query = QueryParser({name: 'John Doe'});
+    
+        it('should return true on doc has equal field', function () {
+            let result = query.match({name: 'John Doe'});
+    
+            expect(result).to.be.true;
+        });
+        
+        it('should return false on doc has no equal field', function () {
+            let result = query.match({name: 'john doe'});
+    
+            expect(result).to.be.false;
+        }); 
+    });
+    
+});
