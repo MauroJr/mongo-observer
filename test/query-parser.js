df7b0e1e01425724ab26cb368f10a68b59c0c121
Mauro Junior <mauropereirajr@gmail.com>
2016-03-17 15:51:05 -0300

    added tests for "$gt" operator

--- test/query-parser.js
+++ test/query-parser.js
@@ -9,13 +9,13 @@ describe('Query Parser', function () {
     describe('query without operators', function () {
         let query = QueryParser({name: 'John Doe'});
     
-        it('should return true on doc has equal field', function () {
+        it('should return true when doc has equal field', function () {
             let result = query.match({name: 'John Doe'});
     
             expect(result).to.be.true;
         });
         
-        it('should return false on doc has no equal field', function () {
+        it('should return false when doc has no equal field', function () {
             let result = query.match({name: 'john doe'});
     
             expect(result).to.be.false;
@@ -25,13 +25,13 @@ describe('Query Parser', function () {
     describe('query without operators with implicit "$and"', function () {
         let query = QueryParser({name: 'John Doe', age: 20, id: 321});
         
-        it('should return true on all fields is equal', function () {
+        it('should return true when all fields is equal', function () {
            let result = query.match({name: 'John Doe', age: 20, id: 321});
            
            expect(result).to.be.true;
         });
         
-        it('should return false on least one field is not equal', function () {
+        it('should return false when least one field is not equal', function () {
            let result = query.match({name: 'John Doe', age: 19, id: 321});
            
            expect(result).to.be.false;
@@ -41,14 +41,30 @@ describe('Query Parser', function () {
     describe('query with "$eq" operator', function () {
         let query = QueryParser({age: {$eq: 18}});
         
-        it('should return true on doc has equal field', function () {
+        it('should return true when doc has equal field', function () {
             let result = query.match({name: 'John Doe', age: 18});
     
             expect(result).to.be.true;
         });
         
-        it('should return false on doc has no equal field', function () {
+        it('should return false when doc has no equal field', function () {
             let result = query.match({name: 'John Doe', age: 22});
+    
+            expect(result).to.be.false;
+        });
+    });
+    
+    describe('query with "$gt" operator', function () {
+        let query = QueryParser({age: {$gt: 18}});
+        
+        it('should return true when doc has a higher value', function () {
+            let result = query.match({name: 'John Doe', age: 28});
+    
+            expect(result).to.be.true;
+        });
+        
+        it('should return false when doc has a lower value', function () {
+            let result = query.match({name: 'John Doe', age: 12});
     
             expect(result).to.be.false;
         });
